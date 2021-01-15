842ef5f22cbe30110a4e379e71e2fa810aa8ee18
Celia-01 <74036641+Celia-01@users.noreply.github.com>
2021-01-15 10:44:02 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,11 +1,11 @@
 <？php
     error_reporting（0）;
     
-    $ conn = mysqli_connect（ “ localhost”， “ root”， “”， “ mydb”）;
+    $ conn = mysqli_connect（ “ localhost”， “ root”， “”， “ mydb”）;//建立資料庫連線，如果$conn=false代表連結不成功
     如果（mysqli_connect_error（$ conn））
         die（“資料庫連線錯誤”）;
 
-    mysqli_set_charset（$ conn，“ utf8”）;
+    mysqli_set_charset（$ conn，“ utf8”）;//設置字符集
     $ result = mysqli_query（ $ conn， “從用戶選擇*”）;
     
     $ login = FALSE ;
@@ -16,7 +16,7 @@
     
     if   （！$ _POST [ “ id” ] ||！！$ _POST [ “ pwd” ]）{
            迴聲 “請輸入帳號/密碼”；
-           迴聲 “ <meta http-equiv ='refresh'content ='3; url = login.html''>” ;              
+           迴聲 “ <meta http-equiv ='refresh'content ='3; url = login.html''>” ;//上面例子會讓網頁在載入 3 秒後，自動跳轉到login.html              
     }
     elseif（$ login == TRUE）{
       session_start（）;
