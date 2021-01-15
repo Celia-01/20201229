914065e0708ca79f03831ff13bfa33161e65b1b0
Celia-01 <74036641+Celia-01@users.noreply.github.com>
2021-01-15 10:42:00 +0800

    Update 8.logout.php

--- 8.logout.php
+++ 8.logout.php
@@ -4,6 +4,6 @@ session_start（）;
 //將會話清空
 unset（$ _SESSION [ “ id” ]）;
 迴聲 “登出中......” ;
-迴聲 '<meta http-equiv = REFRESH CONTENT = 2; url = login.html>' ;
+迴聲 '<meta http-equiv = REFRESH CONTENT = 2; url = login.html>' ;//上面例子會讓網頁在載入 2 秒後，自動跳轉到login.html
 
 ？>
