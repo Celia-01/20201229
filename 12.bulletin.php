4c21180267ed2b563b47062074ba537783a4a311
Celia-01 <74036641+Celia-01@users.noreply.github.com>
2021-01-15 10:47:20 +0800

    Update 12.bulletin.php

--- 12.bulletin.php
+++ 12.bulletin.php
@@ -2,11 +2,11 @@
     error_reporting（0）;
     session_start（）;
     if（！isset（$ _SESSION [ “ id” ]））{
-        迴聲 “請登錄系統”；
+        迴聲 “請登錄系統”；//這行會被echo出來文字「請登入系統」
         迴聲 “ <meta http-equiv ='refresh'content ='3; url = login.html'>” ;
     }其他{
         echo  “歡迎{$ _SESSION ['id']}}登錄[<a href=logout.php>登出</a>] <p> [<a href=bulletin_add_form.php>添加佈告</a>] < p>”；
-        $ conn = mysqli_connect（ “ localhost”， “ root”， “”， “ mydb”）;
+        $ conn = mysqli_connect（ “ localhost”， “ root”， “”， “ mydb”）;//建立資料庫連線，如果$conn=false代表連結不成功
         $ result = mysqli_query（ $ conn， “從公告中選擇*”）；
         echo  “ <表邊框= 2> <tr> <td>佈告操作</ td> <td>佈告編號</ td> <td>佈告類別</ td> <td>標題</ td> <td>佈告內容</ td> <td>發佈時間</ td> </ tr>”；
         while（$ row = mysqli_fetch_array（$ result））{
